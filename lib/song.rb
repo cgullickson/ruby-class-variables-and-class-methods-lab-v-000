9312863339463efea412e449d71777b99d9be014
Chris Gullickson <cgullickson5@gmail.com>
2017-11-29 16:21:55 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,12 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+
+  def initialize (song, artist, genre)
+    @song = song
+    @artist = artist
+    @genre = genre
+    @@count += 1
+  end
+  
