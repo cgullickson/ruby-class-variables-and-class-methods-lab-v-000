e252ad3614f56d53eead33bb11e9d30fa919b82a
Chris Gullickson <cgullickson5@gmail.com>
2017-11-29 16:46:39 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,7 +32,7 @@ class Song
       if genre_hash.has_key?(genre)
         genre_hash[genre] += 1
       else
-        genre_hash[genre] = ""
+        genre_hash[genre] = []
         genre_hash[genre] = 1
       end
     end
