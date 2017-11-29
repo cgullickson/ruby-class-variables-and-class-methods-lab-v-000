51f0ecf7f84c3b5eeece4eafdd23ef8046c8efd1
Chris Gullickson <cgullickson5@gmail.com>
2017-11-29 16:38:22 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,5 +26,21 @@ class Song
     @@genres.uniq
   end
 
+  def self.genre_count
+    genre_hash = {}
+    @@genres.each do |genre|
+      if !genre_hash.include?(genre)
+        genre_hash[genre] = []
+        genre_hash[genre] = [1]
+      else
+        genre_hash[genre] += 1
+      end
+    end
+  end
+  
+          
+        
+      
+
 
 end
